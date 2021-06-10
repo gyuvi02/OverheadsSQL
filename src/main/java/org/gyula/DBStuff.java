a5e3e171e18246ff5a795d08fd41fdcee3c61272
gyuvi02 <Gyulus99>
2021-06-10 12:33:34 +0200

    Tha giant MonthlyData class completed

--- src/main/java/org/gyula/DBStuff.java
+++ src/main/java/org/gyula/DBStuff.java
@@ -27,9 +27,29 @@ public class DBStuff {
     }
 
     public static MonthlyData getDataForInvoice(String address) throws SQLException {
-        ResultSet resultsLakas = createStatement().executeQuery("select * from " + Constants.TABLE_LAKAS + " WHERE CIM = " + address);
-        MonthlyData md = new MonthlyData(address, resultsLakas.getString("LAKO"), resultsLakas.getString("LAKBER"), resultsLakas.getString("KOZOSKOLTSEG"),
-                resultsLakas.getString("GAZALAPDIJ"), resultsLakas.getString("VILLANYALAPDIJ"), 0.1,);
+        ResultSet resultsLakas = createStatement().executeQuery(
+                "SELECT DISTINCT l.LAKO, l.LAKBER, l.KOZOSKOLTSEG, l.GAZALAPDIJ, l.VILLANYALAPDIJ, " +
+                        "(SELECT DATUM FROM (SELECT g.DATUM FROM A_GAZORA g ORDER BY g.datum DESC) WHERE ROWNUM <= 1) AS \"GORAALLAS_DATUM\", " +
+                        "(SELECT ORAALLAS FROM (SELECT g.ORAALLAS FROM A_GAZORA g ORDER BY g.datum DESC) WHERE ROWNUM <= 1) AS \"REGI_GORAALLAS\", " +
+                        "(SELECT datum FROM (SELECT v.DATUM FROM A_VILLANYORA v ORDER BY v.datum DESC) WHERE ROWNUM <= 1) AS \"VORAALLAS_DATUM\"," +
+                        "(SELECT ORAALLAS FROM (SELECT v.oraallas FROM A_VILLANYORA v ORDER BY v.datum DESC) WHERE ROWNUM <= 1) AS \"REGI_VILLANYORAALLAS\", " +
+                        "e.GAZ_AR, e.VILLANY_AR" +
+                        " FROM (SELECT * FROM " + Constants.TABLE_LAKAS + " WHERE CIM ='" + address + "'" + ")" + " l " +
+                " JOIN " + Constants.TABLE_EGYSEGARAK + " e ON e.LAKASID = l.ID " +
+                " JOIN " + Constants.TABLE_GAZORA + " g ON g.LAKASID = l.ID " +
+                " JOIN " + Constants.TABLE_VILLANYORA + " v ON v.LAKASID = l.ID ");
+
+
+        resultsLakas.next();
+
+        MonthlyData md = new MonthlyData(address, resultsLakas.getString("LAKO"), resultsLakas.getDouble("LAKBER"),
+                resultsLakas.getDouble("KOZOSKOLTSEG"), resultsLakas.getDouble("GAZALAPDIJ"),
+                resultsLakas.getDouble("VILLANYALAPDIJ"), resultsLakas.getDouble("REGI_GORAALLAS"),
+                resultsLakas.getDate("GORAALLAS_DATUM") ,
+                resultsLakas.getDouble("REGI_VILLANYORAALLAS"),
+                resultsLakas.getDate("VORAALLAS_DATUM"),
+                resultsLakas.getDouble("GAZ_AR"),
+                resultsLakas.getDouble("VILLANY_AR"));
         createStatement().close();
         return md;
     }
--- src/main/java/org/gyula/MonthlyData.java
+++ src/main/java/org/gyula/MonthlyData.java
@@ -1,5 +1,7 @@
 package org.gyula;
 
+import java.sql.Date;
+
 public class MonthlyData {
     private String address;
     private String subtenant;
@@ -9,12 +11,16 @@ public class MonthlyData {
     private double baseFeeElectricity;
     private double gasLast;
     private double gasPresent;
+    private java.sql.Date gasLastDate;
     private double electricityLast;
     private double electrictiyPresent;
-
+    private java.sql.Date electricityLastDate;
+    private double gasCostPerUnit;
+    private double electricityCostPerUnit;
 
     public MonthlyData(String address, String subtenant, double rent, double commonCosts, double baseFeeGas, double baseFeeElectricity,
-                       double gasLast, double gasPresent, double electricityLast, double electrictiyPresent) {
+                       double gasLast, java.sql.Date gasLastDate, double electricityLast, Date electricityLastDate,
+                       double gasCostPerUnit, double electricityCostPerUnit) {
         this.address = address;
         this.subtenant = subtenant;
         this.rent = rent;
@@ -23,7 +29,67 @@ public class MonthlyData {
         this.baseFeeElectricity = baseFeeElectricity;
         this.gasLast = gasLast;
         this.gasPresent = 0.0;
+        this.gasLastDate = gasLastDate;
         this.electricityLast = electricityLast;
         this.electrictiyPresent = 0.0;
+        this.electricityLastDate = electricityLastDate;
+        this.gasCostPerUnit = gasCostPerUnit;
+        this.electricityCostPerUnit = electricityCostPerUnit;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getSubtenant() {
+        return subtenant;
+    }
+
+    public double getRent() {
+        return rent;
+    }
+
+    public double getCommonCosts() {
+        return commonCosts;
+    }
+
+    public double getBaseFeeGas() {
+        return baseFeeGas;
+    }
+
+    public double getBaseFeeElectricity() {
+        return baseFeeElectricity;
+    }
+
+    public double getGasLast() {
+        return gasLast;
+    }
+
+    public double getGasPresent() {
+        return gasPresent;
+    }
+
+    public Date getGasLastDate() {
+        return gasLastDate;
+    }
+
+    public double getElectricityLast() {
+        return electricityLast;
+    }
+
+    public double getElectrictiyPresent() {
+        return electrictiyPresent;
+    }
+
+    public Date getElectricityLastDate() {
+        return electricityLastDate;
+    }
+
+    public double getGasCostPerUnit() {
+        return gasCostPerUnit;
+    }
+
+    public double getElectricityCostPerUnit() {
+        return electricityCostPerUnit;
     }
 }
